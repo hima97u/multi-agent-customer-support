f89599b165ff0ec29cd599d4abb9f244fbd8d76c
hima97u <work.himan7u@gmail.com>
2026-08-04 19:24:00 +0530

    Added orders_list view bu using login_required decorator and also added logout functionallity

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Order
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+@login_required
+def orders_list(request):
+    orders = Order.objects.filter(user=request.user)
+    context = {
+        'orders': orders,
+    }
+    return render(request, 'orders_list.html', context)
