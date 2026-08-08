eddae8990d54b9a8a5e798e7ccb2b178a87c4303
hima97u <work.himan7u@gmail.com>
2026-08-08 20:59:22 +0530

    get reply from backend and fix JS bugs

--- support/views.py
+++ support/views.py
@@ -1,9 +1,15 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 import json
+import time
 
 def chat(request,order_id): # comig from order detail sendMessage() fxn
     if request.method == 'POST':
         data = json.loads(request.body)
         user_message = data.get("message")
 
-        print("i/p msg ->" , user_message)
+        if not user_message:
+            return JsonResponse({"error": "Message cannot be empty."}, status=400)
+
+        time.sleep(6)
+        return JsonResponse({"reply":"Here is the reply"})
